0664f1227e8f4ea90c26d1bd2dd8e82ff2c454fa
David Martinez Alcantara <martinezalcantara29@gmail.com>
2022-09-06 21:42:16 +0200

    cosas pal dron

---
+++ Assets/Drone/DroneCanvasC.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroneCanvasC : MonoBehaviour
+{
+    private Vector2 LstartButton; //start position of left joystick
+
+    private Vector2 RButton; //Right joystick
+    private Vector2 LButton; //left joystick
+    private Vector2 RstartButton; //start position of right joystick
+
+    public bool RL; //RL==true means that it is right joysctick, RL=false means that it is left joystick
+
+    public float Rx;
+    public float Ry;
+    public float Lx;
+    public float Ly;
+
+    private int ForwardBackward;
+    private int Tilt;
+    private int FlyLeftRight;
+    private int UpDown;
+    public GameObject Drone;
+
+
+    void Start()
+    {
+        LButton = transform.position;
+        RButton = transform.position;
+        LstartButton = transform.position;
+        RstartButton = transform.position;
+        if (RL == true)
+        {
+            RstartButton = RButton;
+        }
+
+        if (RL == false)
+        {
+            LstartButton = LButton;
+        }
+    }
+
+    void Update()
+    {
+        LButton = transform.position;
+        RButton = transform.position;
+        ForwardBackward = Drone.GetComponent<DroneControlC>().forwardBackwardForce;
+        Tilt = Drone.GetComponent<DroneControlC>().panForce;
+        FlyLeftRight = Drone.GetComponent<DroneControlC>().leftRightForce;
+        UpDown = Drone.GetComponent<DroneControlC>().UpDownForce;
+    }
+
+    public void Drag()
+    {
+        if (RL)
+        {
+            // Send difference between start position and current position of right joystick
+            Rx = RButton.x - RstartButton.x; 
+            Ry = RButton.y - RstartButton.y;
+            if (Rx > 0)
+            {
+                Rx = Rx + FlyLeftRight + 50;
+            }
+            if (Rx < 0)
+            {
+                Rx = Rx - FlyLeftRight - 50;
+            }
+            
+            if (Ry > 0)
+            {
+                Ry = Ry + UpDown + 50;
+            }
+            if (Ry < 0)
+            {
+                Ry = Ry - UpDown - 50;
+            }
+        }
+        else
+        {
+            // Send difference between start position and current position of left joystick(X)
+            Lx = LButton.x - LstartButton.x;
+            if (Lx > 0)
+            {
+                Lx = Lx + Tilt + 40;
+            }
+
+            if (Lx < 0)
+            {
+                Lx = Lx - Tilt - 40;
+            }
+
+            // Send difference between start position and current position of left joystick(Y)
+            Ly = LButton.y - LstartButton.y; 
+            if (Ly > 0)
+            {
+                Ly = Ly + ForwardBackward + 40;
+            }
+
+            if (Ly < 0)
+            {
+                Ly = Ly - ForwardBackward - 40;
+            }
+        }
+    }
+
+    public void endDrag()
+    {
+        if (RL)
+        {
+            RButton = RstartButton;
+            Rx = Ry = 0;
+        }
+        else
+        {
+            LButton = LstartButton;
+            Lx = Ly = 0;
+        }
+    }
+}
---
+++ Assets/Drone/DroneControlC.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroneControlC : MonoBehaviour {
+	public Rigidbody Drone;
+	public GameObject RButton;
+	public GameObject LButton;
+
+		
+	   /*Speed*/public int forwardBackwardForce = 50; 
+	   /*Speed*/public int panForce = 50; 
+	   /*Speed*/public int leftRightForce = 50;  
+	   /*Speed*/public int UpDownForce = 50; 
+
+	public Vector3 DroneRotation;
+	public bool Mobile;
+	private float Rx;
+	private float Ry;
+	private float Lx;
+	private float Ly;
+
+	public float maxTorque = 10;
+	public float enginePower = 100;
+
+	void Update () {
+		if (Mobile) 
+		{
+			Rx = RButton.GetComponent<DroneCanvasC>().Rx;
+			Ry = RButton.GetComponent<DroneCanvasC>().Ry;
+			Lx = LButton.GetComponent<DroneCanvasC>().Lx;
+			Ly = LButton.GetComponent<DroneCanvasC>().Ly;
+		}
+	}
+
+	void FixedUpdate () {
+		DroneRotation = Drone.transform.localEulerAngles;
+
+		enginePower = 100;
+		
+		// STABILIZATION Z axis
+		if (DroneRotation.z > maxTorque && DroneRotation.z <= 180)
+		{
+			Drone.AddRelativeTorque (0, 0, -maxTorque);
+		}
+		else if (DroneRotation.z > 180 && DroneRotation.z <= 360 - maxTorque)
+		{
+			Drone.AddRelativeTorque (0, 0, maxTorque);
+		}
+		
+		if (DroneRotation.z > 1 && DroneRotation.z <= maxTorque)
+		{
+			Drone.AddRelativeTorque (0, 0, -3);
+		}
+		else if (DroneRotation.z > maxTorque && DroneRotation.z <= 359)
+		{
+			Drone.AddRelativeTorque (0, 0, 3);
+		}
+
+		
+		// STABILIZATION X axis
+		if (DroneRotation.x > maxTorque && DroneRotation.x <= 180)
+		{
+			Drone.AddRelativeTorque (Vector3.right * -maxTorque);
+		}
+		else if (DroneRotation.x > 180 && DroneRotation.x <= 360 - maxTorque)
+		{
+			Drone.AddRelativeTorque (Vector3.right * maxTorque);
+		}
+
+		if (DroneRotation.x > 1 && DroneRotation.x <= 10)
+		{
+			Drone.AddRelativeTorque (Vector3.right * -3);
+		}
+		else if (DroneRotation.x > 350 && DroneRotation.x < 359)
+		{
+			Drone.AddRelativeTorque (Vector3.right * 3);
+		}
+
+		
+		// PAN
+		if(Mobile)
+		{
+			Drone.AddRelativeTorque(Vector3.up * Lx / 5);
+		}
+		else
+		{
+			// LEFT
+			if (Input.GetKey(KeyCode.A))
+			{
+				Drone.AddRelativeTorque(-Vector3.up * panForce);
+			}
+			// RIGHT
+			if (Input.GetKey(KeyCode.D))
+			{
+				Drone.AddRelativeTorque(Vector3.up * panForce);
+			}
+		}
+
+		// MOVEMENT
+		if(Mobile)
+		{
+			Drone.AddForce(transform.forward * Ly/2);
+			if (Ly > 5)
+			{
+				Drone.AddRelativeTorque (maxTorque, 0, 0);
+			}
+			else if (Ly < -5)
+			{
+				Drone.AddRelativeTorque (-maxTorque, 0, 0);
+			}
+
+			Drone.AddRelativeForce(Rx,0,0);if(Rx>5){Drone.AddRelativeTorque (0, 0,-10);};if(Rx<-5){Drone.AddRelativeTorque (0, 0,10);}
+
+
+			Drone.AddRelativeForce(0,Ry/2,0);//drone fly up or down
+		}
+		else 
+		{
+			// FORWARD
+			if(Input.GetKey(KeyCode.W))
+			{
+				enginePower += forwardBackwardForce;
+				//Drone.AddForce(transform.forward * forwardBackwardForce);
+				Drone.AddRelativeTorque (Vector3.right * maxTorque);
+			}
+			
+			// BACKWARD
+			if (Input.GetKey(KeyCode.S))
+			{
+				enginePower += forwardBackwardForce;
+				//Drone.AddForce(transform.forward * -forwardBackwardForce);
+				Drone.AddRelativeTorque (Vector3.right * -maxTorque);
+			}
+
+			// LEFT
+			if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				enginePower += leftRightForce;
+				//Drone.AddForce(transform.right * -leftRightForce);
+				Drone.AddRelativeTorque (Vector3.forward * maxTorque);
+			}
+
+			// RIGHT
+			if (Input.GetKey(KeyCode.RightArrow))
+			{
+				enginePower += leftRightForce;
+				//Drone.AddForce(transform.right * leftRightForce);
+				Drone.AddRelativeTorque (Vector3.forward * -maxTorque);
+			}
+			
+			// UP
+			if (Input.GetKey(KeyCode.UpArrow))
+			{
+				enginePower += UpDownForce;
+				//Drone.AddRelativeForce(Vector3.up * UpDown);
+			}
+			
+			// DOWN
+			if (Input.GetKey(KeyCode.DownArrow))
+			{
+				enginePower -= UpDownForce;
+				//Drone.AddRelativeForce(Vector3.up * -UpDown);
+			}
+		}
+		
+		enginePower = Physics.gravity.magnitude / Mathf.Cos(Mathf.Max(DroneRotation.x, DroneRotation.z) * Mathf.Deg2Rad);
+		
+		// HOVER
+		Drone.AddRelativeForce(Vector3.up * enginePower);
+	}
+
+}
